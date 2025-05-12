d1eb6e79085012ababe9d3581b5447baba4332a1
vinceamarinn <vicenteamorimsilva@gmail.com>
2025-05-12 15:21:49 +0100

    Finished data module
    Polished the data module a little more, added an external plugin which *should* deter at least some arbitrary code execution attempts through loaded save data, and added safe checks to the option resource to unallow property sets to go over or under the predefined range.

--- scripts/modules/DataStateHandler.gd
+++ scripts/modules/DataStateHandler.gd
@@ -18,7 +18,9 @@ func save_data(data_file:Resource) -> bool: ## Saves the game.
 
 func load_data(data_file:Resource) -> bool: ## Loads selected save data file.
 	if not ResourceLoader.exists(get_data_path(data_file)): return false # if we cant find it, return false
-	var loaded_data = ResourceLoader.load(get_data_path(data_file))
+	
+	var loaded_data = SafeResourceLoader.load(get_data_path(data_file)) # use safe resource loading plugin to check for malicious code
+	if loaded_data == null: return false # bad code found, don't run
 	
 	for property in loaded_data.get_property_list():
 		if not "type" in property or not (property["usage"] & PROPERTY_USAGE_SCRIPT_VARIABLE): continue # filters out anything that isn't an explicitly defined variable in the resource
@@ -30,4 +32,27 @@ func load_data(data_file:Resource) -> bool: ## Loads selected save data file.
 
 func _ready() -> void:
 	ServiceLocator.register_service("DataModule", self) # registers module in service locator automatically
-	print("CurrentChapter" in game_data)
+	
+	# TESTING
+	print(game_data.CurrentChapter)
+	print(option_data.MusicVolume)
+	
+	set_property(game_data, "CurrentChapter", 3)
+	
+	print("saving game...")
+	var savegame = save_data(game_data)
+	print("saving successful? ", savegame)
+	print("loading game...")
+	var loadgame = load_data(game_data)
+	print("loading successful? ", loadgame)
+	
+	set_property(option_data, "MusicVolume", -2)
+	set_property(option_data, "SFXVolume", 3)
+	set_property(option_data, "VoiceVolume", 0.5)
+	
+	print("saving options...")
+	var saveoption = save_data(option_data)
+	print("saving successful? ", saveoption)
+	print("loading options...")
+	var loadoption = load_data(option_data)
+	print("loading successful? ", loadoption)
--- scripts/resources/OptionData.gd
+++ scripts/resources/OptionData.gd
@@ -5,8 +5,23 @@ const RESOURCE_NAME = "OptionData"
 
 @export_group("General Settings")
 @export_subgroup("Volume")
-@export_range(0, 1, 0.01) var MusicVolume:float = 1 ## Changes the overall volume of background music. Default is 100%.
-@export_range(0, 1, 0.01) var SFXVolume:float = 1 ## Changes the overall volume of sound effects. Default is 100%.
-@export_range(0, 1, 0.01) var VoiceVolume:float = 1 ## Changes the overall volume of voicelines. Default is 100#.
+var music_volume:float = 1
+var sfx_volume:float = 1
+var voice_volume:float = 1
+
+@export_range(0, 1, 0.01) var MusicVolume:float: ## Changes the overall volume of background music. Default is 100%.
+	get: return music_volume
+	set(value):
+		music_volume = clamp(value, 0, 1)
+
+@export_range(0, 1, 0.01) var SFXVolume:float: ## Changes the overall volume of sound effects. Default is 100%.
+	get: return sfx_volume
+	set(value):
+		sfx_volume = clamp(value, 0, 1)
+
+@export_range(0, 1, 0.01) var VoiceVolume:float: ## Changes the overall volume of voicelines. Default is 100#.
+	get: return voice_volume
+	set(value):
+		voice_volume = clamp(value, 0, 1)
 
 @export_group("Keybinds")
