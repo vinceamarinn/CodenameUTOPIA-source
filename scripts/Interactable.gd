a3bf74ae40ce19eff157e46892066548b18c6911
vinceamarinn <vicenteamorimsilva@gmail.com>
2025-07-01 20:06:34 +0100

    On interaction mode & ironing out the quirks
    ADDITIONS
    - finished the on_interact mode for the interactables and added a placeholder interactable prompt to signify that you're in range.
    CHANGES
    Interactables now have two extra variables:
    >is_within_range: used for the on_interact mode to detect whether or not the player is in range of the interactable so that it can execute its action.
    >interact_fired: serves as a failsafe that indicates the execute_action function is already running so you can't spam interact prompts to do the same thing over and over.

--- scripts/Interactable.gd
+++ scripts/Interactable.gd
@@ -11,11 +11,18 @@ enum InteractAction {
 	LOAD_AREA, ## Calls the area module to load a new area.
 }
 
+var is_within_range:bool = false ## Determines whether or not the player is within an interactable's range, in case it matters for activation conditions.
+var interactable_fired:bool = false ## Locks the code from running again if the interactable has already been ran.
+
 @export var interactable_type:InteractableType ## Type of interaction that triggers the interactable. being met.
 @export var interact_action:InteractAction ## Action executed upon meeting the interaction requirements.
 @export var action_data:Dictionary[String, Variant] ## Data to pass as arguments for the chosen action.
 
+@onready var interaction_prompt:SpriteBase3D = $Prompt ## The prompt.
+
 func execute_action():
+	if interactable_fired: return
+	interactable_fired = true
 	
 	match interact_action:
 		InteractAction.PRINT_TEXT:
@@ -29,6 +36,17 @@ func execute_action():
 		_:
 			pass
 
+func _input(event: InputEvent) -> void: # handle input for on_interact interactables
+	if event.is_action_pressed("interact") and is_within_range == true:
+		execute_action()
+
+func _ready() -> void:
+	# set up the interactable!!!
+	if interactable_type == InteractableType.ON_TOUCH: return # no need to do this if it's a non interact interactable
+	var interactable_parent = self.get_parent()
+	interaction_prompt.position.y += interactable_parent.mesh.size.y/2 + 1 # position it accordingly to the size of the parent object
+	interaction_prompt.visible = false
+
 func _on_body_entered(body: Node3D) -> void:
 	if not body is PlayerOverworld: return
 	
@@ -36,7 +54,8 @@ func _on_body_entered(body: Node3D) -> void:
 		InteractableType.ON_TOUCH:
 			execute_action()
 		InteractableType.ON_INTERACT:
-			print("should create popup to trigger event on interaction")
+			is_within_range = true
+			interaction_prompt.visible = true
 		_:
 			pass
 
@@ -44,4 +63,5 @@ func _on_body_exited(body: Node3D) -> void:
 	if not body is PlayerOverworld: return
 	
 	if interactable_type == InteractableType.ON_INTERACT:
-		print("should destroy popup")
+		is_within_range = false
+		interaction_prompt.visible = false
