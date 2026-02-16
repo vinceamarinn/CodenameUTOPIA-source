017b5015e666e81545b39a0ce7c94c94d6163c82
vinceamarinn <vicenteamorimsilva@gmail.com>
2026-02-16 20:34:51 +0000

    Tiny Camera Module fixes
    MAJOR
    >The Follow Player mode now uses curr_cam_subject instead of its own player tracker in order to standardize its functionality with the rest of the module.
    >As a consequence, the mode is now much more flexible, allowing you to theoretically follow around *any* 3D object and not just the player character.
    
    MINOR
    >Reorganized the initial structure a little better.
    >Fixed a couple misleading comments, and added some missing comments.

--- scripts/modules/CameraModule.gd
+++ scripts/modules/CameraModule.gd
@@ -12,7 +12,7 @@ extends Node
 var curr_cam_tween:Tween = null
 var curr_cam_subject:Node3D = null
 
-# important stuff
+# camera mode stuff
 enum CameraModes { ## lists the different modes for the camera.
 	NONE, ## The camera is static and does nothing. Allows for custom behavior to be executed without interruption.
 	FOLLOW_PLAYER, ## The camera follows the player around smoothly.
@@ -20,8 +20,10 @@ enum CameraModes { ## lists the different modes for the camera.
 	DIALOGUE ## The camera is set to tween smoothly to focus on which character is talking during Overworld dialogue sections.
 }
 
-# define initial positions for each different Trial camera option
-var position_table = {
+var camera_mode:CameraModes = CameraModes.NONE ## Tracks the currently selected camera mode.
+
+# table storing the initial positions for each different Trial camera option
+const POSITION_TABLE = {
 	"Pan" = {
 		"Left" = [Vector3(3, 0, 0), Vector3(-.25, 0, 0)],
 		"Right" = [Vector3(-3, 0, 0), Vector3(.25, 0, 0)],
@@ -35,10 +37,7 @@ var position_table = {
 	},
 }
 
-var camera_mode:CameraModes = CameraModes.NONE ## Tracks the currently selected camera mode.
-var player:PlayerOverworld = null ## Tracks the player character so it can be followed around during Follow Player mode.
-
-# values
+# important values
 const FOLLOW_SPEED:float = 5.0 ## Speed at which the camera follows the player in Follow Player mode.
 const FOLLOW_OFFSET:Vector3 = Vector3(0, 2, 8.5) ## Offset with which the camera tracks the player from in Follow Player mode.
 
@@ -62,8 +61,7 @@ func reset_camera() -> void: ## Sets the camera mode to None and resets referenc
 	# kill and reset tween reference
 	kill_current_tween()
 	
-	# reset player & current camera subject references if they exist
-	player = null
+	# current camera subject references if they exist
 	curr_cam_subject = null
 
 func set_mode(chosen_mode:CameraModes) -> void: ## Switches the camera mode to one of the chosen modes.
@@ -72,12 +70,12 @@ func set_mode(chosen_mode:CameraModes) -> void: ## Switches the camera mode to o
 	
 	# if we wanna follow the player, get the player character if it exists - otherwise abort the function and essentially only reset the camera
 	if chosen_mode == CameraModes.FOLLOW_PLAYER:
-		player = char_group.get_node_or_null("Player")
+		curr_cam_subject = char_group.get_node_or_null("Player")
 		
-		if player == null:
+		if curr_cam_subject == null:
 			return
 	
-	# switch to player follow mode
+	# switch to requested camera mode
 	camera_mode = chosen_mode
 
 func _physics_process(delta:float) -> void: ## Fires every frame with the physics engine.
@@ -85,12 +83,12 @@ func _physics_process(delta:float) -> void: ## Fires every frame with the physic
 	if camera_mode != CameraModes.FOLLOW_PLAYER: return
 	
 	# if the player reference is invalid, stop the player follow mode
-	if not is_instance_valid(player) or player == null: 
+	if not is_instance_valid(curr_cam_subject) or curr_cam_subject == null: 
 		reset_camera()
 		return
 	
 	# smoothly follow player with lerping
-	var target_pos = player.position + FOLLOW_OFFSET
+	var target_pos = curr_cam_subject.position + FOLLOW_OFFSET
 	camera.position = camera.position.lerp(target_pos, FOLLOW_SPEED * delta)
 
 func get_initial_transform(movement_type:String, movement_direction:String, base_rotation:int) -> Array[Vector3]: ## Gets the initial position & rotation for Trial mode camera tweens.
@@ -99,7 +97,7 @@ func get_initial_transform(movement_type:String, movement_direction:String, base
 	if movement_type != "Snap":
 		# if it's not a Snap movement (there's actual tweening), the offset will be equal to the base offset + the movement appropriate offset, according to the chosen movement & direction
 		# [0] signifies it's the initial position from the base
-		movement_offset += position_table[movement_type][movement_direction][0]
+		movement_offset += POSITION_TABLE[movement_type][movement_direction][0]
 	# if it's a Snap movement, then it's just the base offset vector, so no change is needed
 	
 	# init_pos is initiated as equal to the camera subject's transform multiplied by the movement's determined offset
@@ -113,7 +111,7 @@ func get_initial_transform(movement_type:String, movement_direction:String, base
 
 func get_final_transform(movement_type:String, movement_direction:String, final_rotation:int) -> Array[Vector3]: ## Gets the final position & rotation for Trial mode camera tweens.
 	# define the movement offset by taking the base offset and matching it to the camera movement type:
-	var movement_offset = TRIAL_BASE_OFFSET + position_table[movement_type][movement_direction][1]
+	var movement_offset = TRIAL_BASE_OFFSET + POSITION_TABLE[movement_type][movement_direction][1]
 	# the offset will be equal to the base offset + the movement appropriate offset, according to the chosen movement & direction
 	# [1] signifies it's the final position from the base
 	
@@ -145,18 +143,17 @@ func dialogue_tween(cam_subject:Node3D) -> void: ## Handles tweening for regular
 	kill_current_tween()
 	
 	# create new tween
-	var new_tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT).set_parallel(true)
+	var new_tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
 	curr_cam_tween = new_tween
 	curr_cam_subject = cam_subject
 	
 	# tween the position
 	new_tween.tween_property(camera, "position", cam_subject.position + DIALOGUE_OFFSET, DIALOGUE_TIME)
 
-func initial_transition(cam_info:CameraMovementData, cam_subject:Node3D) -> void: ## Handles creating the initial transition from the current camera location to the initial position of the tween. Used for the Trial camera mode.
+func initial_transition(cam_info:CameraMovementData) -> void: ## Handles creating the initial transition from the current camera location to the initial position of the tween. Used for the Trial camera mode.
 	# create tween
 	var new_tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN).set_parallel(true)
 	curr_cam_tween = new_tween
-	curr_cam_subject = cam_subject
 	
 	# get initial coordinate frame (pos + rot) based on movement type and initial rotation 
 	var init_cframe = get_initial_transform(cam_info.MovementType, cam_info.MovementDirection, cam_info.BaseRotation)
@@ -180,13 +177,17 @@ func trial_tween(cam_info:CameraMovementData, cam_subject:Node3D) -> void: ## Ha
 	# abort if the camera subject is invalid
 	if not is_instance_valid(cam_subject) or cam_subject == null:
 		GeneralModule.debug_message("CameraModule - trial_tween()", "Error", "Trial tween was cancelled.", "The camera subject was not found in the scene!")
+		return
 	
 	# stop any ongoing tweens
 	kill_current_tween()
 	
+	# set the camera subject (so other tweens don't have to do it themselves)
+	curr_cam_subject = cam_subject
+	
 	# perform initial transition if it was asked of us
 	if cam_info.InitialTransition:
-		await initial_transition(cam_info, cam_subject)
+		await initial_transition(cam_info)
 	
 	# signal that the transition is finished and the actual camera movement has begun
 	transition_finished.emit()
@@ -202,7 +203,6 @@ func trial_tween(cam_info:CameraMovementData, cam_subject:Node3D) -> void: ## Ha
 		# create tween (easings are not set immediately, we'll set them separately per property for a cool effect!)
 		var new_tween = create_tween().set_parallel(true)
 		curr_cam_tween = new_tween
-		curr_cam_subject = cam_subject
 		
 		# get final transform to calculate endpoints of the tween
 		var final_transform = get_final_transform(cam_info.MovementType, cam_info.MovementDirection, cam_info.FinalRotation)
@@ -226,8 +226,6 @@ func trial_tween(cam_info:CameraMovementData, cam_subject:Node3D) -> void: ## Ha
 			final_rot,
 			cam_info.CameraTime
 		).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
-		
-		
 
 func _ready() -> void:
 	ServiceLocator.register_service("CameraModule", self) # registers module in service locator automatically
